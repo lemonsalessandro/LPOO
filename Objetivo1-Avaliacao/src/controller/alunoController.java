23a9c0210e5361cb5360964ddc7792ba0dcdf836
Alessandro <alessandro.lemons@gmail.com>
2023-08-24 17:00:49 -0300

    list e map final

--- Objetivo1-Avaliacao/src/controller/alunoController.java
+++ Objetivo1-Avaliacao/src/controller/alunoController.java
@@ -13,6 +13,16 @@ public class alunoController {
         Aluno aluno5 = new Aluno();
         Aluno aluno6 = new Aluno();
 
+
+        //GETTER E SETTER DAS INSTÂNCIAS COM 2 PARÂMETROS
+        //ALUNO 5
+        aluno5.setId(5);
+        aluno5.setEmail("mariana@gmail.com");
+
+        //ALUNO 6
+        aluno6.setId(6);
+        aluno6.setEmail("douglas@gmail.com");;
+
         //TO STRING
         System.out.println("TO STRING");
         System.out.println("Aluno 1: " + aluno1);
@@ -22,7 +32,7 @@ public class alunoController {
         System.out.println("Aluno 5: " + aluno5);
         System.out.println("Aluno 6: " + aluno6);
 
-        //GETTERS E SETTERS
+        //GETTERS E SETTERS CONSTRUTORES PADRÃO
         System.out.println("\n\nGETTERS E SETTERS");
 
         //ALUNO 1
@@ -51,19 +61,46 @@ public class alunoController {
         System.out.println("Sobrenome: " + aluno2.getSobrenome());
         System.out.println("Email: " + aluno2.getEmail());
 
-        //ALUNO 5
-        aluno5.setId(5);
-        aluno5.setEmail("mariana@gmail.com");
-        System.out.println("\nALUNO 5:");
-        System.out.println("ID: " + aluno5.getId());
-        System.out.println("Email: " + aluno5.getEmail());
 
-        //ALUNO 6
-        aluno6.setId(6);
-        aluno6.setEmail("douglas@gmail.com");
-        System.out.println("\nALUNO 6:");
-        System.out.println("ID: " + aluno6.getId());
-        System.out.println("Email: " + aluno6.getEmail());
+        //LIST
+        List<Aluno> alunosList = new ArrayList<>();
 
+        System.out.println("\nALUNOS LIST: ");
+        alunosList.add(aluno1);
+        alunosList.add(aluno2);
+        alunosList.add(aluno3);
+        alunosList.add(aluno4);
+        alunosList.add(aluno5);
+        alunosList.add(aluno6);
+
+        System.out.println(alunosList);
+
+       Aluno alunoId5 = alunosList.stream().filter(a -> a.getId()==5).findAny().orElse(null);
+       System.out.println("Aluno ID 5 LIST: " + alunoId5);
+
+
+
+        //LIST COM ORDEM DESCRESCENTE
+        alunosList.sort(Comparator.comparing(Aluno::getId).reversed());
+        System.out.println("\nLIST EM ORDEM DECRESCENTE");
+        System.out.println(alunosList);
+
+
+        //MAP
+        System.out.println("\nALUNOS MAP");
+
+        Map<Integer, Aluno> alunosMap = new HashMap<>();
+
+        alunosMap.put(aluno1.getId(), aluno1);
+        alunosMap.put(aluno2.getId(), aluno2);
+        alunosMap.put(aluno3.getId(), aluno3);
+        alunosMap.put(aluno4.getId(), aluno4);
+        alunosMap.put(aluno5.getId(), aluno5);
+        alunosMap.put(aluno6.getId(), aluno6);
+
+        System.out.println(alunosMap);
+
+        //PRINT DO OBJETO ID 5 COM MAP
+        System.out.println("Aluno ID 5 MAP: " + alunosMap.get(aluno5.getId()));
     }
 }
